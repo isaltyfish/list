f74faf76ed37990e5e4e9810b208178167d2a066
saltyfish <isaltyfish@126.com>
2022-10-10 21:24:25 +0800

    命令行选项

---
+++ README.md
@@ -0,0 +1,15 @@
+# 手写命令行
+
+## 运行方法
+
+```shell
+go run main.go -l
+# 或者
+go run main.go --lst
+```
+
+## 教程地址
+
+[https://verytools.net/xtools-guide/posts/easy-command-part2](https://verytools.net/xtools-guide/posts/easy-command-part2)
+
+
--- cmd/root.go
+++ cmd/root.go
@@ -18,28 +18,27 @@ import (
 // rootCmd represents the base command when called without any subcommands
 var rootCmd = &cobra.Command{
 	Use:   "list",
-	Short: "A brief description of your application",
-	Long: `A longer description that spans multiple lines and likely contains
-examples and usage of using your application. For example:
-
-Cobra is a CLI library for Go that empowers applications.
-This application is a tool to generate the needed files
-to quickly create a Cobra application.`,
-	// Uncomment the following line if your bare application
-	// has an action associated with it:
+	Short: "列出当前文件下的内容",
+	Long: `列出当前文件下所有的文件名称，包括文件名和目录名。`,
 	Run: func(cmd *cobra.Command, args []string) {
-		listDirContent()
+		// 注意：需要通过长名称获取选项的值
+		value, _ := cmd.Flags().GetBool("lst")
+		sep := " "
+		if value {
+			sep = "\n"
+		}
+		listDirContent(sep)
 	},
 }
 
-func listDirContent() {
+func listDirContent(sep string) {
 	wd, _ := os.Getwd()
 	files, _ := ioutil.ReadDir(wd)
 	fileNames := make([]string, 0, len(files))
 	for _, file := range files {
 		fileNames = append(fileNames, file.Name())
 	}
-	fmt.Println(strings.Join(fileNames, " "))
+	fmt.Println(strings.Join(fileNames, sep))
 }
 
 // Execute adds all child commands to the root command and sets flags appropriately.
@@ -61,6 +60,5 @@ func init() {
 	// Cobra also supports local flags, which will only run
 	// when this action is called directly.
 	rootCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+	rootCmd.Flags().BoolP("lst", "l", false, "列出文件名，并使用换行符分隔")
 }
-
-
