64a694a5c5d7c6ad4b22229ca883e083fb891af5
saltyfish <isaltyfish@126.com>
2022-10-10 21:30:25 +0800

    list logic

--- cmd/root.go
+++ cmd/root.go
@@ -18,16 +18,10 @@ import (
 // rootCmd represents the base command when called without any subcommands
 var rootCmd = &cobra.Command{
 	Use:   "list",
-	Short: "A brief description of your application",
-	Long: `A longer description that spans multiple lines and likely contains
-examples and usage of using your application. For example:
-
-Cobra is a CLI library for Go that empowers applications.
-This application is a tool to generate the needed files
-to quickly create a Cobra application.`,
-	// Uncomment the following line if your bare application
-	// has an action associated with it:
+	Short: "列出当前文件下的内容",
+	Long: `列出当前文件下所有的文件名称，包括文件名和目录名。`,
 	Run: func(cmd *cobra.Command, args []string) {
+		// 这里写命令行逻辑
 		listDirContent()
 	},
 }
